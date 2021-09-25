b2cac3dddffc2665ee19402be4430874a9d5fc34
RabeyaEty01 <etyrabeya01@gmail.com>
2021-09-25 18:18:17 +0600

    cart added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
+    
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
@@ -12,7 +15,8 @@ const Shop = () => {
     }, [])
 
     const handleAddToCart = (product) => {
-        console.log(product);
+        const newCart=[...cart,product];
+        setCart(newCart);
     }
 
     return (
@@ -28,8 +32,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>Order summery</h3>
-                <h5>Items ordered:</h5>
+               <Cart cart={cart}></Cart>
             </div>
         </div>
     );
