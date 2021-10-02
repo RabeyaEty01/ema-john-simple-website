dc3ba65d3d4e924e906202a8830158999fdc0187
RabeyaEty01 <etyrabeya01@gmail.com>
2021-10-02 22:54:00 +0600

    route added and remove item and order review added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
