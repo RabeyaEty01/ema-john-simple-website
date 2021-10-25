c2cfa7aefa392c034c99587600f6a1db580151ba
RabeyaEty01 <etyrabeya01@gmail.com>
2021-10-25 19:50:02 +0600

    data loaded from node mongo server and shipping added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import useCart from '../../hooks/useCart';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
-import { addToDb, getStoredCart } from '../../utilities/fakedb'
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import './Shop.css';
 import { Link } from 'react-router-dom';
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
     const [displayProducts, setDisplayProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-
+    const [cart, setCart] = useCart();
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0);
+    const size = 10;
     useEffect(() => {
-        fetch('./products.json')
+        fetch(`http://localhost:5000/products?page=${page}&&size=${size}`)
             .then(res => res.json())
             .then(data => {
-                setProducts(data);
-                setDisplayProducts(data);
-
+                setProducts(data.products);
+                setDisplayProducts(data.products);
+                const count = data.count;
+                const pageNumber = Math.ceil(count / size);
+                setPageCount(pageNumber);
             });
-    }, []);
+    }, [page]);
 
 
     useEffect(() => {
@@ -41,7 +46,7 @@ const Shop = () => {
             setCart(storedCart);
         }
 
-    }, [products]);
+    }, []);
 
     const handleAddToCart = (product) => {
         const exists = cart.find(pd => pd.key === product.key);
@@ -49,7 +54,7 @@ const Shop = () => {
         if (exists) {
             const rest = cart.filter(pd => pd.key !== product.key);
             exists.quantity = exists.quantity + 1;
-            newCart=[...rest, exists];
+            newCart = [...rest, exists];
         }
         else {
             product.quantity = 1;
@@ -87,6 +92,16 @@ const Shop = () => {
                             handleAddToCart={handleAddToCart}
                         ></Product>)
                     }
+
+                    <div className="pagination">
+                        {
+                            [...Array(pageCount).keys()].map(number => <button
+                                className={number === page ? 'selected' : ''}
+                                key={number}
+                                onClick={() => setPage(number)}
+                            >{number+1}</button>)
+                        }
+                    </div>
                 </div>
                 <div className="cart-container">
                     <Cart cart={cart}>
